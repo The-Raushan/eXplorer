8b640b45a265a4741717c799da6fb5a9635e84db
The-Raushan <theraushankrx@gmail.com>
2026-03-28 19:18:58 +0530

    Add files via upload

---
+++ eXplorer.js
@@ -0,0 +1,21 @@
+/* Developed By : Raushan Kumar 
+  JS Starting Date : 2026-march-27
+  Last updated : 2026-march-27
+  version : 1.0.0
+  Description : This Java Script file is used to Functions the eXPLORER website. 
+*/
+
+
+for (let i =1; i<= 10; i++){
+    console.log("eXplorer")
+}
+
+
+let obj = {
+    item : "Clothes",
+    price : 2500,
+  
+};
+
+let output = `the cost of ${obj.item} is ${obj.price} rupees `;
+console.log(output);
